abe71c2f056e191ddf03e945678905b697c121c1
puntopunto <usheynet@gmail.com>
2023-01-09 11:53:07 +0300

    init 2

---
+++ Source/PluginBuilder/Public/Ara.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ARActor.h"
+#include "Ara.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINBUILDER_API AAra : public AARActor
+{
+	GENERATED_BODY()
+	vector ARAVector a;
+	
+	
+};
