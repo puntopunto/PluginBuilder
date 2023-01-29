5b416510399da22c7147bbfe4bf5f0a1877880fc
puntopunto <usheynet@gmail.com>
2023-01-29 05:57:04 +0300

    Created new plugin.

---
+++ Source/PluginBuilder/Private/CordsFlow.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CordsFlow.h"
+
+FString input_string = L"This is the string";
+auto substrings = input_string.ParseIntoArrayWS(substrings);
