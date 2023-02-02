a06024b9e1abb4dceebbbc832ee49c91d2124154
puntopunto <usheynet@gmail.com>
2023-02-03 00:56:57 +0300

    Switched to VS

---
+++ Plugins/Developer/RiderLink/Source/RiderBlueprint/Public/Track_InputCords.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Track_InputCords.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RIDERBLUEPRINT_API UTrack_InputCords : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PluginBuilder/Public/XOTrack_CordsFlow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "XOTrack_CordsFlow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINBUILDER_API UXOTrack_CordsFlow : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
