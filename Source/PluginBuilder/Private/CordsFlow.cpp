be8740997d2c78f423e876957871c1bf2f70770a
puntopunto <usheynet@gmail.com>
2023-02-04 22:27:18 +0300

    Preparinf VP scene

--- Source/PluginBuilder/Private/CordsFlow.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CordsFlow.h"
-
-FString input_string = L"This is the string";
-auto substrings = input_string.ParseIntoArrayWS(substrings);
